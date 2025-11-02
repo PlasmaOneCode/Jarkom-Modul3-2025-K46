4a6db80c768efa690c359cd7e3442dd62cf2a1c7
PlasmaOneCode <abiyyurpw@gmail.com>
2025-11-02 21:45:01 +0700

    Delete script directory untuk diganti baru

--- script/9.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# 9.sh - PHP Worker setup
-apt update -y
-apt install -y apache2 php
-
-echo "<?php echo 'Halo dari ' . gethostname(); ?>" > /var/www/html/index.php
-systemctl enable apache2
-systemctl restart apache2
-echo "[✓] PHP Worker siap."
